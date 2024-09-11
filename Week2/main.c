b3bbe359ff62baa8c0729a7b7988f917b68ff91e
002976 <tiffanybyun0209@gmail.com>
2024-09-11 14:32:14 +0900

    week2 assessment

--- Week2/main.c
+++ Week2/main.c
@@ -5,7 +5,13 @@
 													//set function
 int main(int argc, char *argv[]) {
 	
+	int a,b;										//set data type
+	
+	a = 10;											//set data information
+	b = 20;
+	
 	printf("Hello, Sookmyung Electronics!\n"); 		//on-screen
+	printf("%d + %d = %d\n", a, b, a+b);
 	
 	return 0;
 }													//end function
