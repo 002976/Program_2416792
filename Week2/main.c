b0f3a10992499ecf591cbff9b8132779071541e6
002976 <tiffanybyun0209@gmail.com>
2024-09-11 14:29:19 +0900

    week2 assessment

---
+++ Week2/main.c
@@ -0,0 +1,11 @@
+													//set preprocessor 
+#include <stdio.h>
+#include <stdlib.h>
+
+													//set function
+int main(int argc, char *argv[]) {
+	
+	printf("Hello, Sookmyung Electronics!\n"); 		//on-screen
+	
+	return 0;
+}													//end function
